688c70e9b1294a2e4ee3223f9ae532af9e71993e
Semih Eroğlu <65253167+thegears@users.noreply.github.com>
2022-01-27 00:47:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const { MongoDB } = require("ark.db");
+const db = new MongoDB("mongodb+srv://biktim:biktim@cluster0.j62wo.mongodb.net/codears?retryWrites=true&w=majority", "codears");
+
+app.use(express.static("src"));
+
+app.get("/adminpanelsemihicinabijimbulinkikimsebulamazlol",(req,res) => {
+    res.sendFile(__dirname+"/src/adminpanel.html");
+});
+
+app.get("/",(req,res) => {
+    res.sendFile(__dirname+"/src/main.html");
+});
+
+app.get("/:qwd",(req,res) => {
+    res.sendFile(__dirname+"/src/main.html");
+});
+
+
+io.on("connection",(socket) => {
+    socket.on("getUsersToServer",async (data) => {
+        socket.emit("getUsersToClient",await db.get("users"));
+    });
+
+    socket.on("newUserToServer",async data => {
+        await db.push("users",data);
+    });
+
+    socket.on("getCodesToServer",async data => {
+        socket.emit("getCodesToClient",[await db.get("codes"),await db.get("visibilityTrueCodes")]);
+    });
+
+    socket.on("getCodeLikesToServer",async data => {
+        let likes = await db.get(`${data}Likes`);
+        socket.emit("getCodeLikesToClient",likes ? likes : []);
+    });
+
+    socket.on("newLikeToServer",async data => {
+        await db.push(`${data.code}Likes`,data.user);
+    });
+
+    socket.on("newCodeToServer",async data => {
+        let codes = await db.get("codes");
+        data.codeId = codes.length;
+        await db.push("codes",data);
+        await db.push(`visibilityFalseCodes`,codes.length);
+    });
+
+
+
+    socket.on("adminPanelGetCodesToServer",async data => {
+        socket.emit("adminPanelGetCodesToClient",[await db.get("codes"),await db.get("visibilityFalseCodes"),await db.get("sifre")]);
+    });
+
+    socket.on("adminPanelAcceptCodeToServer",async data => {
+        let vfc = await db.get("visibilityFalseCodes");
+        vfc = vfc.filter(a => a != data);
+        await db.set("visibilityFalseCodes",vfc);
+        await db.push("visibilityTrueCodes",data);
+    });
+    
+    socket.on("adminPanelDeclineCodeToServer",async data => {
+        let vfc = await db.get("visibilityFalseCodes");
+        vfc = vfc.filter(a => a != data);
+        await db.set("visibilityFalseCodes",vfc);
+    });
+});
+
+
+server.listen(3000);
---
+++ src/script.js
@@ -0,0 +1,104 @@
+const socket = io();
+
+var _vue = new Vue({
+    el : "#app",
+    data : {
+        page : "main",
+        user : "",
+        codes : [],
+        viewingCodeId : "0",
+        kodHeartColor : "#111",
+        viewingCodeLikes : "",
+        isLiked : "false"
+    },
+    methods : {
+        register(){
+            socket.emit("getUsersToServer","getUsersToServer");
+            socket.on("getUsersToClient",data => {
+                $ = (a) => document.querySelector(a);
+                if($("#registerNickname").value.split("").length == $("#registerNickname").value.split("").filter(a => a == " ").length || $("#registerUsername").value.split("").length == $("#registerUsername").value.split("").filter(a => a == " ").length || $("#registerPassword").value.split("").length == $("#registerPassword").value.split("").filter(a => a == " ").length) return alert("Lütfen Boş Yer Bırakmayın ! ");
+                if($("#registerNickname").value.split("").length < 3) return alert("Takma ad en az 3 karakterli olmalı ! ");
+                if($("#registerUsername").value.split("").length < 6) return alert("Kullanıcı adı en az 6 karakterli olmalı ! ");
+                if($("#registerPassword").value.split("").length < 8) return alert("Şifre en az 8 karakterli olmalı ! ");
+                if(data){
+                    if(data.map(a => a.nickName).includes($("#registerNickname").value)) return alert("Bu Takma Ad Kullanılmaktadır ! ");
+                    if(data.map(a => a.userName).includes($("#registerUsername").value)) return alert("Bu Kullanıcı Adı Kullanılmaktadır ! ");
+    
+                    socket.emit("newUserToServer",{
+                        nickName : $("#registerNickname").value,
+                        userName : $("#registerUsername").value,
+                        password : $("#registerPassword").value
+                    });
+
+                    this.user = $("#registerNickname").value;
+
+                    this.page = "main";
+                };
+            });
+        },
+        login(){
+            $ = (a) => document.querySelector(a);
+            if($("#loginUsername").value.split("").length == $("#loginUsername").value.split("").filter(a => a == " ").length || $("#loginPassword").value.split("").length == $("#loginPassword").value.split("").filter(a => a == " ").length) return alert("Lütfen Boş Yer Bırakmayın ! ");
+            
+            socket.emit("getUsersToServer","getUsersToServer");
+            socket.on("getUsersToClient",data => {
+                if(!data.map(a => a.userName).includes($("#loginUsername").value) || $("#loginPassword").value != data[data.map(a=>a.userName).indexOf($("#loginUsername").value)].password) return alert("Kullanıcı adı veya şifre yanlış ! ");
+            
+                this.user = data[data.map(a=>a.userName).indexOf($("#loginUsername").value)].nickName;
+
+                this.page = "main";
+            });
+        },
+        getCodes(){
+            socket.emit("getCodesToServer","getCodesToServer");
+            socket.on("getCodesToClient",data => {
+                this.codes = data[0].filter(a => data[1].includes(a.codeId));
+            });
+        },
+        viewCode(i){
+            this.page = "kod";
+            socket.emit("getCodeLikesToServer",i);
+            socket.on("getCodeLikesToClient",data => {
+                this.viewingCodeLikes = data;
+                this.viewingCodeId = i;
+                if(data.includes(this.user)){
+                    this.kodHeartColor = "red";
+                }else{
+                    this.kodHeartColor = "#111";
+                };
+            });
+        },
+        likeCode(){
+            if(this.isLiked == "true" || this.viewingCodeLikes.includes(this.user)) return alert("Zaten beğenmişsiniz ! ");
+            this.isLiked = "true";
+            this.viewingCodeLikes.push(this.user);
+            let codeId = this.viewingCodeId;
+            let user = this.user;
+            socket.emit("newLikeToServer",{
+                code : codeId,
+                user : user
+            });
+            this.kodHeartColor = "red";
+        },
+        addCode(){
+            $ = (a) => document.querySelector(a);
+
+            if($("#kodEkleCodeName").value.split("").length == $("#kodEkleCodeName").value.split("").filter(a => a == " ").length  || $("#kodEkleCodeArea").value.split("").length == $("#kodEkleCodeArea").value.split("").filter(a => a == " ").length) return alert("Lütfen Boş Yer Bırakmayın ! ");
+            if($("#kodEkleCodeName").value.split("").length < 10) return alert("Kod ismi en az 10 karakterli olmalıdır ! ");
+            if($("#kodEkleCodeDescription").value.split("").length < 20) return alert("Kod açıklaması en az 20 karakterli olmalıdır ! ");
+
+            let user = this.user;
+
+            socket.emit("newCodeToServer",{
+                name:$("#kodEkleCodeName").value,
+                description:$("#kodEkleCodeDescription").value,
+                owner:user,
+                code:$("#kodEkleCodeArea").value,
+                codeId : ""
+            });
+
+            alert("Kodunuz onaylandıktan sonra yayınlanacaktır.");
+            this.page = "main";
+        }
+    }
+});
